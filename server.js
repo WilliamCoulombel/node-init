b140340b3fdbd0e8628cc8aa7eae5b7f1e9c1f37
William Coulombel <w.coulombel@gmail.com>
2020-12-08 15:48:46 +0100

    relancement du serveur

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ let express = require('express');
 let app = express();
 let port = 3000;
 
+app.use(express.static(__dirname+'/www'));
+app.use('/js', express.static(__dirname+'/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname+'/node_modules/jquery/dist'));
+app.use('/js', express.static(__dirname+'/node_modules/popper.js/dist/umd'));
+app.use('/js', express.static(__dirname+'/node_modules/bootstrap/dist/css'));
+
 app.listen(port, () => {
     console.log('le serveur est en route');
     console.log(`Serveur listenning at https://localhost:${port}`);
 })
 
 app.get('/',(req, res, next)=>{
-    res.send('Bonjour !');
+    res.send('Bonjour tout le monde, je ne vous aime pas ! ceci arrivera instantanément');
 });
